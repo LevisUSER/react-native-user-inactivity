b2c74a29be4cfe83caaf79c27efc245c9c938572
Levis Luong <36833446+LevisUSER@users.noreply.github.com>
2020-08-14 15:11:15 +0700

    Add throttle to panresponder

--- src/index.tsx
+++ src/index.tsx
@@ -153,6 +153,38 @@ const UserInactivity: React.FC<UserInactivityProps> = ({
     };
   }, []);
 
+  function throttle(func, wait, options) {
+        var context, args, result;
+        var timeout = null;
+        var previous = 0;
+        if (!options) options = {};
+        var later = function() {
+          previous = options.leading === false ? 0 : Date.now();
+          timeout = null;
+          result = func.apply(context, args);
+          if (!timeout) context = args = null;
+        };
+        return function() {
+          var now = Date.now();
+          if (!previous && options.leading === false) previous = now;
+          var remaining = wait - (now - previous);
+          context = this;
+          args = arguments;
+          if (remaining <= 0 || remaining > wait) {
+            if (timeout) {
+              clearTimeout(timeout);
+              timeout = null;
+            }
+            previous = now;
+            result = func.apply(context, args);
+            if (!timeout) context = args = null;
+          } else if (!timeout && options.trailing !== false) {
+            timeout = setTimeout(later, remaining);
+          }
+          return result;
+        };
+      };
+  
   /**
    * This method is called whenever a touch is detected. If no touch is
    * detected after `this.props.timeForInactivity` milliseconds, then
@@ -181,13 +213,11 @@ const UserInactivity: React.FC<UserInactivityProps> = ({
   /**
    * The PanResponder instance is initialized only once.
    */
-  const [panResponder, _] = useState(
-    PanResponder.create({
-      onMoveShouldSetPanResponderCapture: resetTimerForPanResponder,
-      onPanResponderTerminationRequest: resetTimerForPanResponder,
-      onStartShouldSetPanResponderCapture: resetTimerForPanResponder,
-    }),
-  );
+  const [panResponder, _] = useState(PanResponder.create({
+        onMoveShouldSetPanResponderCapture: throttle(resetTimerForPanResponder,1000),
+        onPanResponderTerminationRequest: throttle(resetTimerForPanResponder,1000),
+        onStartShouldSetPanResponderCapture: throttle(resetTimerForPanResponder,1000),
+    }));
 
   return (
     <View
